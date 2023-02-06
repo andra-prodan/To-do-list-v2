60bb1e46b6a1f1d7d97ea9ee5cb5a352ceff06ce
andra-prodan <andra.ioana.prodan@gmail.com>
2023-02-07 00:48:58 +0200

    Modifing for deploy

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const https = require("https");
 const date = require(__dirname + "/date.js");
 const mongoose = require("mongoose");
 const _ = require("lodash");
+const process = require('dotenv').config();
+
 
 mongoose.set("strictQuery", false);
 
